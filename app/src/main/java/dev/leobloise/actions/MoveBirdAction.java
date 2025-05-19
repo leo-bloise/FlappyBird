c5868020bf74eabcbb3978a3d1fee19438f79f84
Leonardo Bloise <leonardocsbloise@gmail.com>
2025-05-18 22:10:59 -0300

    feat: add sound to bird move.

--- app/src/main/java/dev/leobloise/actions/MoveBirdAction.java
+++ app/src/main/java/dev/leobloise/actions/MoveBirdAction.java
@@ -1,14 +1,19 @@
 package dev.leobloise.actions;
 
 import dev.leobloise.components.Bird;
+import dev.leobloise.entities.GameAudio;
 
 public class MoveBirdAction implements GameAction{
     private Bird bird;
+    private final GameAudio gameAudio;
     public MoveBirdAction(Bird bird) {
         this.bird = bird;
+        this.gameAudio = new GameAudio("wing.wav");
+        gameAudio.read();
     }
     @Override
     public synchronized void execute() {
         bird.move();
+        gameAudio.play();
     }
 }
